60d479ead2205d042abe52b7367e3f00d81cf466
laolj <2910963774@qq.com>
2022-07-07 15:22:09 +0800

    增删改查，一对一多表查询，分页查询

--- src/main/java/com/example/demo1/mapper/UserVoTestRepository.java
+++ src/main/java/com/example/demo1/mapper/UserVoTestRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+/**
+ * 继承JpaRepository 实现分页增删改查功能
+ * 继承JpaSpecificationExecutor 实现复杂的多表查询功能
+ */
 @Repository
 public interface UserVoTestRepository extends JpaRepository<UserVO, Long>, JpaSpecificationExecutor<UserVO> {
 
